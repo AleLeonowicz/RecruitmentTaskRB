ff5a65cd9c2a99ce3edcc1eb30799680dba763f3
AleLeonowicz <aleks.leonowicz@gmail.com>
2022-07-23 17:02:43 +0200

    Creates mapping in prodcts list

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,25 +1,34 @@
-import React, { useContext } from 'react';
-import StateContext from '../../store/state-context';
+import React from 'react';
 import classes from './ListItem.module.scss';
 
-import img0001 from '../../assets/mocks/img0001.jpeg';
-// import img0002 from '../assets/mocks/img0002';
-
-const ListItem = () => {
-  const { fetchedData } = useContext(StateContext);
-
+const ListItem = props => {
   return (
     <div className={classes.listItem_container}>
       <div className={classes.listItem_box}>
-        <img src={img0001} alt={fetchedData[0].image.alt}></img>
-        <h5>{fetchedData[0].product_type}</h5>
-        <h4>{fetchedData[0].title}</h4>
-        <p>Rating: {fetchedData[0].rating.toString()}</p>
-        <h1>{fetchedData[0].price.toString()} €</h1>
-        <button>DETAILS</button>
+        <div className={classes.listItem_imgBlock}>
+          <img src={props.src} alt={props.alt} />
+        </div>
+        <div className={classes.listItem_textBlock}>
+          <p>{props.productType}</p>
+          <h4>{props.title}</h4>
+          <p>Rating: {props.rating}</p>
+          <h1>{props.price} €</h1>
+          <button className={classes.addToCartBtn}>Add to cart</button>
+        </div>
       </div>
     </div>
   );
 };
 
 export default ListItem;
+
+/* <div className={classes.listItem_container}>
+<div className={classes.listItem_box}>
+  <img src={img0001} alt={fetchedData[0].image.alt}></img>
+  <h5>{fetchedData[0].product_type}</h5>
+  <h4>{fetchedData[0].title}</h4>
+  <p>Rating: {fetchedData[0].rating.toString()}</p>
+  <h1>{fetchedData[0].price.toString()} €</h1>
+  <button>DETAILS</button>
+</div>
+</div> */
--- src/components/ProductsList/ProductsList.js
+++ src/components/ProductsList/ProductsList.js
@@ -2,17 +2,23 @@ import React from 'react';
 
 import ListItem from '../ListItem/ListItem';
 import classes from './ProductsList.module.scss';
+import { productsMock } from '../../mocks/index';
 
 const ProductsList = () => {
   return (
     <div className={classes.productsList_container}>
-      <ListItem />
-      <ListItem />
-      <ListItem />
-      <ListItem />
-      <ListItem />
-      <ListItem />
-      <ListItem />
+      {productsMock.data.map(el => {
+        return (
+          <ListItem
+            src={el.image.src}
+            alt={el.image.alt}
+            productType={el.product_type}
+            title={el.title}
+            rating={el.rating.toString()}
+            price={el.price.toString()}
+          />
+        );
+      })}
     </div>
   );
 };
